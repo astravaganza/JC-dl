9178a72aeb3584e992390bb0ea9596fcdc9295fc
astravaganza <83530103+astravaganza@users.noreply.github.com>
2022-01-31 21:49:24 +0530

    Added OTP logging system and other code changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, toml, json
+import requests, json
 import os, sys
 
 # define paths
@@ -8,16 +8,65 @@ dirPath = os.path.dirname(realPath)
 dirName = os.path.basename(dirPath)
 ytdl_path = dirPath + "\yt-dlp.exe"
 
-# load config
-config = toml.load("config.toml")
-
 # define 
-ssotoken = config ["ssotoken"]
-uniqueID = config ["uniqueID"]
-Request_URL = config ["Request_URL"]
-Meta_URL = config ["MetaURL"]
+def load_config():
+    global ssotoken, uniqueID
+    with open ("creds.txt", "r") as f:
+        try:
+            Creds = json.load(f)
+            ssotoken = Creds['ssotoken']
+            uniqueID = Creds['uniqueID']
+        except json.JSONDecodeError:
+            ssotoken = ''
+            uniqueID = ''    
+
+Request_URL = "https://prod.media.jio.com/apis/common/v3/playbackrights/get/"
+Meta_URL = "https://prod.media.jio.com/apis/common/v3/metamore/get/"
 #cachePath = config ["cachePath"] 
 #outPath = config ["outPath"]
+OTPSendURL = "https://prod.media.jio.com/apis/common/v3/login/sendotp"
+OTPVerifyURL = "https://prod.media.jio.com/apis/common/v3/login/verifyotp"
+
+def login(mobile_number):
+    send = requests.post(url = OTPSendURL, headers = {
+    'authority': 'prod.media.jio.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'origin': 'https://www.jiocinema.com',
+    'referer': 'https://www.jiocinema.com/',
+    },
+     data = '{"number":"+91' + mobile_number +'"}'
+    )
+    if 'success' in str(send.content):
+        OTP = input ('Enter OTP Received: ')
+        verify = requests.post(url = OTPVerifyURL, headers = {
+        'authority': 'prod.media.jio.com',
+        'pragma': 'no-cache',
+        'origin': 'https://www.jiocinema.com',
+        'referer': 'https://www.jiocinema.com/',
+        'deviceid': '1727391720'
+        },
+        data = '{"number":"+91' + mobile_number + '","otp":"' + OTP + '"}')
+        creds = json.loads(verify.content)
+        print (creds)
+        load_creds(creds)
+    else:
+        print ("Wrong/Unregistered Mobile Number (ensure there's no +91 or 0 in the beginning)")
+        sys.exit()
+
+def load_creds(creds):
+    try:
+        ssotoken = creds['ssoToken']
+        uniqueID = creds['uniqueId']
+    except KeyError:
+        print ("Wrong OTP, Try again!")
+        sys.exit()
+    Creds = {
+        "ssotoken" : ssotoken,
+        "uniqueID" : uniqueID
+    }
+    with open("creds.txt", "w") as f:
+        f.write(json.dumps(Creds))
 
 def get_manifest(VideoID):
     headers = {
@@ -50,7 +99,12 @@ def get_metadata(VideoID):
     return json.loads(response.text)
 
 print ('JioCinema Content Downloading Tool')
-VideoID = input ("Enter VideoID: ")
+load_config()
+if ssotoken == "" and uniqueID == "":
+    M_No = input ('Enter Mobile Number: ')
+    login (M_No)
+    load_config()
+VideoID = input ('Enter VideoID: ')
 manifest = get_manifest(VideoID)
 metadata = get_metadata(VideoID)
 try:
@@ -67,6 +121,7 @@ def get_streams(m3u8):
     os.system(f'{ytdl_path} {m3u8} --allow-unplayable-formats --downloader aria2c --user-agent "JioOnDemand/1.5.2.1 (Linux;Android 4.4.2) Jio" -q --no-warnings') # + -P TEMP:{cachePath} -P HOME:{outPath})
     os.rename(f'{dirPath}\chunklist [chunklist].mp4', fileName)
     print ("\nSuccessfully downloaded the stream!")
+
 
 m3u8_url = get_m3u8(manifest)
 nonDRM_m3u8_url = mod_m3u8(m3u8_url)
