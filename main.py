039b9233a5d7839b1ba9c7db016f38be4a360973
astravaganza <83530103+astravaganza@users.noreply.github.com>
2022-01-31 15:44:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ fileName = f'{content_name}.{metadata["year"]}.mp4'
 def get_streams(m3u8):
     print ("Downloading A/V")
     os.system(f'{ytdl_path} {m3u8} --allow-unplayable-formats --downloader aria2c --user-agent "JioOnDemand/1.5.2.1 (Linux;Android 4.4.2) Jio" -q --no-warnings ')
-    os.rename(f'{outputPath}\chunklist [chunklist].mp4', fileName)
+    os.rename(f'{dirPath}\chunklist [chunklist].mp4', fileName)
     print ("\nSuccessfully downloaded the stream!")
 
 m3u8_url = get_m3u8(manifest)
