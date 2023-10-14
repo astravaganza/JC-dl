b1c218f884561a3721131992451fa4bc0a47be71
astravaganza <83530103+astravaganza@users.noreply.github.com>
2023-10-14 11:34:36 +0530

    updated login logic to v4

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests, json
 import os, sys
+import base64
 
 # define paths
 currentFile = __file__
@@ -22,31 +23,70 @@ def load_config():
 
 Request_URL = "https://prod.media.jio.com/apis/common/v3/playbackrights/get/"
 Meta_URL = "https://prod.media.jio.com/apis/common/v3/metamore/get/"
-OTPSendURL = "https://prod.media.jio.com/apis/common/v3/login/sendotp"
-OTPVerifyURL = "https://prod.media.jio.com/apis/common/v3/login/verifyotp"
+OTPSendURL = "https://auth-jiocinema.voot.com/userservice/apis/v4/loginotp/send"
+OTPVerifyURL = "https://auth-jiocinema.voot.com/userservice/apis/v4/loginotp/verify"
+IdURL = "https://cs-jv.voot.com/clickstream/v1/get-id"
+GuestURL = "https://auth-jiocinema.voot.com/tokenservice/apis/v4/guest"
+
+def get_accesstoken():
+    id = requests.get(url=IdURL).json()['id']
+
+    token = requests.post(url=GuestURL, json={
+            'adId': id,
+            "appName": "RJIL_JioCinema",
+            "appVersion": "23.10.13.0-841c2bc7",
+            "deviceId": id,
+            "deviceType": "phone",
+            "freshLaunch": True,
+            "os": "ios"
+        }, headers={
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+        }).json()
+
+    return token["authToken"], id
 
 def login(mobile_number):
-    send = requests.post(url = OTPSendURL, headers = {
-    'authority': 'prod.media.jio.com',
-    'pragma': 'no-cache',
-    'cache-control': 'no-cache',
-    'origin': 'https://www.jiocinema.com',
-    'referer': 'https://www.jiocinema.com/',
-    },
-     data = '{"number":"+91' + mobile_number +'"}'
-    )
-    if 'success' in str(send.content):
+    accesstoken, id = get_accesstoken()
+    
+    send = requests.post(url=OTPSendURL, json={
+            "number": base64.b64encode(f"+91{mobile_number}".encode()).decode(),
+            "appVersion": "23.10.13.0-841c2bc7"
+        }, headers = {
+            'accesstoken': accesstoken,
+            'appname': 'RJIL_JioCinema',
+            'cache-control': 'no-cache',
+            'devicetype': 'phone',
+            'os': 'ios',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+})
+    print(send.content)
+    if 'karix' in str(send.content):
         OTP = input ('Enter OTP Received: ')
         verify = requests.post(url = OTPVerifyURL, headers = {
-        'authority': 'prod.media.jio.com',
-        'pragma': 'no-cache',
-        'origin': 'https://www.jiocinema.com',
-        'referer': 'https://www.jiocinema.com/',
-        'deviceid': '1727391720'
-        },
-        data = '{"number":"+91' + mobile_number + '","otp":"' + OTP + '"}')
+            'accesstoken': accesstoken,
+            'appname': 'RJIL_JioCinema',
+            'cache-control': 'no-cache',
+            'devicetype': 'phone',
+            'os': 'ios',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+        }, json={
+            "appVersion": "23.10.13.0-841c2bc7",
+            "deviceInfo": {
+                "consumptionDeviceName": "iPhone",
+                "info": {
+                    "androidId": id,
+                    "platform": {
+                        "name": "iPhone OS"
+                    },
+                    "type": "iOS"
+                }
+            },
+            "number": base64.b64encode(f"+91{mobile_number}".encode()).decode(),
+            "otp": OTP
+        })
+        print(verify.content)
         creds = json.loads(verify.content)
-        print (creds)
+        
         load_creds(creds)
     else:
         print ("Wrong/Unregistered Mobile Number (ensure there's no +91 or 0 in the beginning)")
